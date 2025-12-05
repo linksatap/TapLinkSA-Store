2f2e11286984f2593f54bfee07312d2b06c6a5ad
linksatap <linksatap@gmail.com>
2025-12-05 22:08:38 +0300

    #

--- taplinksa-frontend/pages/checkout.js
+++ taplinksa-frontend/pages/checkout.js
@@ -609,10 +609,10 @@ className="bg-white rounded-2xl shadow-lg p-4 sm:p-6 md:p-8"
               )}
             </motion.form>
 
-            {/*<CouponInput 
+            <CouponInput 
               onApplyCoupon={handleApplyCoupon} 
               subtotal={subtotal}
-            />*/}
+            />
           </div>
 
           <div className="lg:col-span-1">
